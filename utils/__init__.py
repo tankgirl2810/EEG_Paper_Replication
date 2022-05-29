6bd68e28722107d0a82066cf4eaae998dd8f27bf
Matthew Lee <matthew.lee@smartaiconnect.com>
2022-05-29 13:03:52 +1000

    Initial training experiment for neural network

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .load_data import load_eeg_data
+from .load_data import load_eeg_data, load_band_training
 
-__all__ = ["load_eeg_data"]
+__all__ = ["load_eeg_data", "load_band_training"]
--- utils/load_data.py
+++ utils/load_data.py
@@ -12,3 +12,14 @@ def load_eeg_data(path: str) -> pd.DataFrame:
         pd.DataFrame: EEG data.
     """
     return pd.read_csv(join(path, 'EEG.machinelearing_data_BRMH.csv'), index_col=0)
+
+def load_band_training(path: str) -> pd.DataFrame:
+    """ Loads band training data from a csv file.
+
+    Args:
+        path (str): Path to the csv file.
+
+    Returns:
+        pd.DataFrame: Band training data.
+    """
+    return pd.read_csv(join(path, 'band_training.csv'))
